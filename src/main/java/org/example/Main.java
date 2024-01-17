63f0313aa7a0fc62a3adfd55afa93097345af68e
Anandhi2211 <142535917+Anandhi2211@users.noreply.github.com>
2024-01-16 21:21:30 -0500

    initail

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("new dev!");
+        System.out.println("asdfasdf dev!");
     }
 }
