310e0a432deb18b030020dee8fef4e23f57b7a3b
apk998 <146481462+apk998@users.noreply.github.com>
2024-01-23 12:11:32 -0500

    Add field

--- src/main/java/com/solvd/bankapp/domain/Account.java
+++ src/main/java/com/solvd/bankapp/domain/Account.java
@@ -1,6 +1,7 @@
 package com.solvd.bankapp.domain;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class Account {
 
@@ -10,10 +11,9 @@ public class Account {
 
     private String username;
 
-    public Account(long accountNumber, BigDecimal totalBalance, String username) {
-        this.accountNumber = accountNumber;
-        this.totalBalance = totalBalance;
-        this.username = username;
+    private ArrayList<Transaction> transactionList;
+
+    public Account() {
     }
 
     public long getAccountNumber() {
@@ -38,6 +38,20 @@ public class Account {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public ArrayList<Transaction> getTransactionList() {
+        if(this.transactionList==null){
+            this.transactionList = new ArrayList<>();
+        }
+        return this.transactionList;
+    }
+
+    public void setTransactionList(Transaction transaction) {
+        if(this.transactionList==null){
+            this.transactionList = new ArrayList<>();
+        }
+        this.transactionList.add(transaction);
     }
 
     @Override
