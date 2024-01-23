c6b811732388fd6160c368f05bf2d8a1ddb0a09d
aparnab4 <manishabalel@gmail.com>
2024-01-23 09:49:15 -0800

    initail

--- src/main/java/com/solvd/bankapp/Main.java
+++ src/main/java/com/solvd/bankapp/Main.java
@@ -2,7 +2,6 @@ package com.solvd.bankapp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("daaa sdfsdf!");
-        System.out.println("aparna");
+
     }
 }
